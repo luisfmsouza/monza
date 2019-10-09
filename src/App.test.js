42afbf7c6c9392e16a4ee2e2c23d246740b24304
Luís Felipe Souza <luisfmsouza@gmail.com>
2019-10-08 23:00:42 -0300

    Add enzyme and enzyme-adapter-react-16 packages
    - Create `src/setupTests.js` configuration file;
    - Create a simple test to make sure Enzyme is working properly.

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import { shallow } from "enzyme";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+import App from "./App";
+
+it("renders without crashing", () => {
+  const wrapper = shallow(<App />);
+
+  expect(wrapper.find(".App-logo").length).toBe(1);
+  expect(wrapper.find("p").text()).toBe("Edit src/App.js and save to reload.");
+  expect(wrapper.find(".App-link").length).toBe(1);
+  expect(wrapper.find(".App-link").prop("href")).toBe("https://reactjs.org");
 });
